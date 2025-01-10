326485ebf647a0f6c68d6f5b86850699129acbee
Sergey Osernovsky <sergey@osernovsky.ru>
2025-01-10 20:57:53 +0300

    Test commit

--- mySnake/Functions/RandomColor.swift
+++ mySnake/Functions/RandomColor.swift
@@ -10,8 +10,10 @@ import UIKit
 // MARK: Функция выдающая случайный цвет (не используется)
 
 func generateRandomColor() -> UIColor {
+    
     let hue : CGFloat = CGFloat(arc4random() % 256) / 256
     let saturation : CGFloat = CGFloat(arc4random() % 128) / 256 + 0.5
     let brightness : CGFloat = CGFloat(arc4random() % 128) / 256 + 0.5
+    
     return UIColor(hue: hue, saturation: saturation, brightness: brightness, alpha: 1)
 }
